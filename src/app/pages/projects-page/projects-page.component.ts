790453f579440b4a6e47af5432e0c4e221c76427
LucHighwalker <luc.highwalker@gmail.com>
2018-12-25 18:36:25 -0800

    - Updated projects.

--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -1,25 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {Project} from "../../project/project.service";
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../../project/project.service';
 
 @Component({
-    selector: 'app-projects-page',
-    templateUrl: './projects-page.component.html',
-    styleUrls: ['./projects-page.component.css']
+  selector: 'app-projects-page',
+  templateUrl: './projects-page.component.html',
+  styleUrls: ['./projects-page.component.css']
 })
 export class ProjectsPageComponent implements OnInit {
+  public projects: {}[];
 
-    private projects: {}[];
+  constructor(private projService: Project) {}
 
-    constructor(private projService: Project) {}
+  ngOnInit() {
+    this.projService.getJSON().subscribe(data => {
+      this.projects = JSON.parse(data._body);
+    });
+  }
 
-    ngOnInit() {
-        this.projService.getJSON().subscribe(data => {
-            this.projects = JSON.parse(data._body);
-        })
-    }
-
-    closeProjects() {
-        this.projService.closeProjects();
-    }
-
+  closeProjects() {
+    this.projService.closeProjects();
+  }
 }
