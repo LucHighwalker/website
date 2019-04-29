98e9297e0fd62dbb344ae70d1614794c79c376c2
LucHighwalker <luc.highwalker@gmail.com>
2019-04-29 13:03:29 -0700

    - Nav/copyright switch whether on mobile/desktop

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -20,8 +20,8 @@ import {
         animate(
           '1.25s ease-in',
           keyframes([
-            style({ opacity: 0, transform: 'translateY(100px)', offset: 0 }),
-            style({ opacity: 1, transform: 'translateY(0)', offset: 1 })
+            style({ opacity: 0, offset: 0 }),
+            style({ opacity: 1, offset: 1 })
           ])
         )
       ])
